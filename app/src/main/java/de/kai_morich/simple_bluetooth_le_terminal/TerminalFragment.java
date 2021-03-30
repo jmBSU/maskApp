d787aa1fa96202ce131b9b9dd68fba940e2d98b3
jmBSU <joema@u.boisestate.edu>
2021-03-29 21:05:50 -0600

    Upload

---
+++ app/src/main/java/de/kai_morich/simple_bluetooth_le_terminal/TerminalFragment.java
@@ -0,0 +1,277 @@
+package de.kai_morich.simple_bluetooth_le_terminal;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.text.Editable;
+import android.text.Spannable;
+import android.text.SpannableStringBuilder;
+import android.text.method.ScrollingMovementMethod;
+import android.text.style.ForegroundColorSpan;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class TerminalFragment extends Fragment implements ServiceConnection, SerialListener {
+
+    private enum Connected { False, Pending, True }
+
+    private String deviceAddress;
+    private SerialService service;
+
+    private TextView receiveText;
+    private TextView receiveText1;
+    private TextView receiveText2;
+
+    private Connected connected = Connected.False;
+    private boolean initialStart = true;
+    private boolean hexEnabled = false;
+    private boolean pendingNewline = false;
+    private String newline = TextUtil.newline_crlf;
+
+    /*
+     * Lifecycle
+     */
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+        setRetainInstance(true);
+        deviceAddress = getArguments().getString("device");
+    }
+
+    @Override
+    public void onDestroy() {
+        if (connected != Connected.False)
+            disconnect();
+        getActivity().stopService(new Intent(getActivity(), SerialService.class));
+        super.onDestroy();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        if(service != null)
+            service.attach(this);
+        else
+            getActivity().startService(new Intent(getActivity(), SerialService.class)); // prevents service destroy on unbind from recreated activity caused by orientation change
+    }
+
+    @Override
+    public void onStop() {
+        if(service != null && !getActivity().isChangingConfigurations())
+            service.detach();
+        super.onStop();
+    }
+
+    @SuppressWarnings("deprecation") // onAttach(context) was added with API 23. onAttach(activity) works for all API versions
+    @Override
+    public void onAttach(@NonNull Activity activity) {
+        super.onAttach(activity);
+        getActivity().bindService(new Intent(getActivity(), SerialService.class), this, Context.BIND_AUTO_CREATE);
+    }
+
+    @Override
+    public void onDetach() {
+        try { getActivity().unbindService(this); } catch(Exception ignored) {}
+        super.onDetach();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if(initialStart && service != null) {
+            initialStart = false;
+            getActivity().runOnUiThread(this::connect);
+        }
+    }
+
+    @Override
+    public void onServiceConnected(ComponentName name, IBinder binder) {
+        service = ((SerialService.SerialBinder) binder).getService();
+        service.attach(this);
+        if(initialStart && isResumed()) {
+            initialStart = false;
+            getActivity().runOnUiThread(this::connect);
+        }
+    }
+
+    @Override
+    public void onServiceDisconnected(ComponentName name) {
+        service = null;
+    }
+
+    /*
+     * UI
+     */
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_terminal, container, false);
+        receiveText = view.findViewById(R.id.receive_text);                          // TextView performance decreases with number of spans
+        receiveText.setTextColor(getResources().getColor(R.color.colorRecieveText)); // set as default color to reduce number of spans
+
+        receiveText1 = view.findViewById(R.id.receive_text_1);                          // TextView performance decreases with number of spans
+        receiveText1.setTextColor(getResources().getColor(R.color.colorRecieveText)); // set as default color to reduce number of spans
+
+        receiveText2 = view.findViewById(R.id.receive_text_2);                          // TextView performance decreases with number of spans
+        receiveText2.setTextColor(getResources().getColor(R.color.colorRecieveText)); // set as default color to reduce number of spans
+
+        return view;
+    }
+
+    @Override
+    public void onCreateOptionsMenu(@NonNull Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.menu_terminal, menu);
+
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.clear) {
+            receiveText.setText("");
+            receiveText1.setText("");
+            receiveText2.setText("");
+            return true;
+        }  else if (id == R.id.send_start) {
+            String startString = "START";
+            send(startString);
+            return true;
+        }  else {
+            return super.onOptionsItemSelected(item);
+        }
+    }
+
+    /*
+     * Serial + UI
+     */
+    private void connect() {
+        try {
+            BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+            BluetoothDevice device = bluetoothAdapter.getRemoteDevice(deviceAddress);
+            //status("connecting...");
+            connected = Connected.Pending;
+            SerialSocket socket = new SerialSocket(getActivity().getApplicationContext(), device);
+            service.connect(socket);
+        } catch (Exception e) {
+            onSerialConnectError(e);
+        }
+    }
+
+    private void disconnect() {
+        connected = Connected.False;
+        service.disconnect();
+    }
+
+    private void send(String str) {
+        if(connected != Connected.True) {
+            Toast.makeText(getActivity(), "not connected", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        try {
+            String msg;
+            byte[] data;
+            msg = str;
+            data = (str + newline).getBytes();
+            SpannableStringBuilder spn = new SpannableStringBuilder(msg+'\n');
+            spn.setSpan(new ForegroundColorSpan(getResources().getColor(R.color.colorSendText)), 0, spn.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+            service.write(data);
+        } catch (Exception e) {
+            onSerialIoError(e);
+        }
+    }
+
+
+    private void receive(byte[] data) {
+
+        String msg = new String(data);
+        if(newline.equals(TextUtil.newline_crlf) && msg.length() > 0) {
+            // don't show CR as ^M if directly before LF
+            msg = msg.replace(TextUtil.newline_crlf, TextUtil.newline_lf);
+            // special handling if CR and LF come in separate fragments
+            if (pendingNewline && msg.charAt(0) == '\n') {
+                Editable edt = receiveText.getEditableText();
+                if (edt != null && edt.length() > 1)
+                    edt.replace(edt.length() - 2, edt.length(), "");
+            }
+            pendingNewline = msg.charAt(msg.length() - 1) == '\r';
+
+            String delimits = "[\n]";
+            String tempString[] = msg.split(delimits);
+
+            for (String s: tempString) {
+                if (s.contains("Temp:")){
+                    String tmpmsg = s.replace("Temp:", "");
+                    //receiveText.append(TextUtil.toCaretString(tmpmsg, newline.length() != 0));
+                    receiveText.setText(TextUtil.toCaretString(tmpmsg, newline.length() != 0));
+                }
+
+                if (s.contains("HR:")){
+                    String tmpmsg = s.replace("HR:", "");
+                    receiveText1.setText(TextUtil.toCaretString(tmpmsg, newline.length() != 0));
+
+                }
+
+                if (s.contains("SPO2:")){
+                    String tmpmsg = s.replace("SPO2:", "");
+                    receiveText2.setText(TextUtil.toCaretString(tmpmsg, newline.length() != 0));
+
+                }
+            }
+
+
+        }
+    }
+
+    private void status(String str) {
+        SpannableStringBuilder spn = new SpannableStringBuilder(str+'\n');
+        spn.setSpan(new ForegroundColorSpan(getResources().getColor(R.color.colorStatusText)), 0, spn.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+        receiveText.setText(spn);
+        receiveText1.setText(spn);
+        receiveText2.setText(spn);
+    }
+
+    /*
+     * SerialListener
+     */
+    @Override
+    public void onSerialConnect() {
+        //status("connected");
+        connected = Connected.True;
+    }
+
+    @Override
+    public void onSerialConnectError(Exception e) {
+        status("connection failed: " + e.getMessage());
+        disconnect();
+    }
+
+    @Override
+    public void onSerialRead(byte[] data) {
+        receive(data);
+    }
+
+    @Override
+    public void onSerialIoError(Exception e) {
+        status("connection lost: " + e.getMessage());
+        disconnect();
+    }
+
+}
